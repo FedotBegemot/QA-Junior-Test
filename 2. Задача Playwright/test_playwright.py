6453283cfef5ce4c0c72921a4d0e166506cc89df
FedotBegemot <93824780+FedotBegemot@users.noreply.github.com>
2025-09-28 21:25:28 +0300

    Update test_playwright.py

--- 2. Задача Playwright/test_playwright.py
+++ 2. Задача Playwright/test_playwright.py
@@ -4,15 +4,13 @@ from playwright.sync_api import sync_playwright
 URL = "https://playwright.dev/"
 EXPECTED_TITLE = "Playwright: Fast and reliable end-to-end testing for modern web apps"
 
-# Тестовая функция, параметризованная браузерами
 @pytest.mark.parametrize("browser_name", ["chromium", "firefox"])
 def test_page_title(browser_name):
     with sync_playwright() as p:
-        # Запуск выбранного браузера
         browser = getattr(p, browser_name).launch()
         page = browser.new_page()
         page.goto(URL)
-        # Проверка заголовка
         title = page.title()
         assert title == EXPECTED_TITLE
+
         browser.close()
